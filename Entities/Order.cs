17424a15169bc8060383af09828bb05f5f72a637
Nullean <mr000lean@gmail.com>
2022-12-05 18:16:38 +0400

    Init commit; created basic entities

---
+++ Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Order
+    {
+        public Guid GuidId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
